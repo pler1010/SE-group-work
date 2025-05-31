7472ac1a580d89ab2e0c4cd89d2ce866fd329bbe
凉意 <133518782+LYcrisp@users.noreply.github.com>
2025-05-31 22:31:11 +0800

    分心和疲劳检测

--- visual.py
+++ visual.py
@@ -2,18 +2,9 @@ from flask import Blueprint, render_template, request, jsonify, session, redirec
 import numpy as np
 import cv2
 from db_utils import get_user_by_id, log_interaction
+from visual_detect.main_detect import detect_fatigue, video_convert2_images
 
 def detect_visual(video):
-    '''
-    需要做的事情：
-    把video中2.5、7.5、12.5（2.5+5n）位置的图像取出来，并且识别对应位置的神态，同时记录日志。
-    '''
-    '''
-    返回值，一个列表：
-    神态有且只有以下几种类型
-    none（没有明显异常）
-    wander（用户走神了）
-    tired（用户有困意）
-    sleepy（用户快睡着了）
-    '''
-    return ['none','none','none','tired','none']
+    video_convert2_images(video, r'./visual_detect/images/run/{}.jpg')
+    info = detect_fatigue(r'./visual_detect/run/1', r'./visual_detect/images/run_result')
+    return info
